78b8616ed26ff540c37a62527fa49e87fd1e63ec
zzero0522 <zzero0522@gmail.com>
2016-06-17 01:53:47 +0800

    add video and edit some css

--- js/init.js
+++ js/init.js
@@ -1,15 +1,8 @@
-/*
-	Ex Machina by TEMPLATED
-    templated.co @templatedco
-    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 skel.init({
 	prefix: 'css/style',
 	resetCSS: true,
 	boxModel: 'border',
 	grid: {
 		gutters: 50
-	},
-	
+	}	
 });
