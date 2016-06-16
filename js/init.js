4eca2f15a2a26e1b4c55af8c538dbb8acc861134
zzero0522 <zzero0522@gmail.com>
2016-06-16 19:58:49 +0800

    Big Change

---
+++ js/init.js
@@ -0,0 +1,15 @@
+/*
+	Ex Machina by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+skel.init({
+	prefix: 'css/style',
+	resetCSS: true,
+	boxModel: 'border',
+	grid: {
+		gutters: 50
+	},
+	
+});
